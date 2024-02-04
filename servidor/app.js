579a4812248ef1fc719bd00f54361378436bd3fd
tahayk3 <tahayk3@gmail.com>
2024-02-03 21:12:36 -0600

    Creando el primer servidor

---
+++ servidor/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const servidor = http.createServer((req,res)=>{
+    res.end("Hola mundo");
+});
+
+servidor.listen(3000,()=>{
+    console.log("El servidor esta encendido y escuchando");
+});
